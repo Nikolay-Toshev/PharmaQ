0a4cbf26a2bccd62132b833d8023623385023456
Nikolay Toshev <n.toshev@proton.me>
2024-12-08 15:18:25 +0200

    feat: deploy project with docker

--- PharmaQ/PharmaQ/settings_exam.py
+++ PharmaQ/PharmaQ/settings_exam.py
@@ -151,9 +151,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('index')
 # Mailjet
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_PORT = config('EMAIL_PORT', 1025, cast=int,)
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
