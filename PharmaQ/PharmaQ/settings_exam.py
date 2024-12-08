6d1c5b3b9498c269703121f9a0a7df7d04e9cd31
Nikolay Toshev <n.toshev@proton.me>
2024-12-08 19:03:31 +0200

    fix: changed the docker-compose.yml

--- PharmaQ/PharmaQ/settings_exam.py
+++ PharmaQ/PharmaQ/settings_exam.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         "HOST": os.getenv('DB_HOST'),
-        "PORT": "5434",
+        "PORT": os.getenv('DB_PORT', 5432),
     }
 }
 
