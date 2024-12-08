46ef7c1dfa503d6c8274ceb5cbd209efad240427
Nikolay Toshev <n.toshev@proton.me>
2024-12-08 18:53:27 +0200

    fix: changed the settings_exam

--- PharmaQ/PharmaQ/settings_exam.py
+++ PharmaQ/PharmaQ/settings_exam.py
@@ -151,9 +151,9 @@ LOGOUT_REDIRECT_URL = reverse_lazy('index')
 # Mailjet
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 1025))
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
 
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
